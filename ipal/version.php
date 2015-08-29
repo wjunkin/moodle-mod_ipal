3e548bdcb027a8ab2d86d5e391fe7c417c661bdb
Bill Junkin <junkinwf@eckerd.edu>
2015-08-29 10:58:40 -0400

    Update version number
    Version number updated to 2015082801

--- ipal/version.php
+++ ipal/version.php
@@ -31,9 +31,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new stdClass;
 }
-$plugin->version  = 2015081000;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2015082801;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2014051200;  // Requires this Moodle version.
 $plugin->cron     = 60;           // Period for cron to check this module (secs).
 $plugin->component = 'mod_ipal';      // To check on upgrade, that module sits in correct place.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.1.3 (Build: 2015081000)';
+$plugin->release = '2.1.3 (Build: 2015082801)';
